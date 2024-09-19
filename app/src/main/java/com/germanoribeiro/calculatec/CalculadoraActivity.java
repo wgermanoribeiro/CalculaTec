919cb4f2066943bc4479484c8c58f35dad234005
wgermanoribeiro <w.germanoribeiro@gmail.com>
2024-09-19 20:57:20 -0300

    Adaptando CalculadoraActivity.java para detectar o separador decimal correto, Criada a pasta assets e inserido o arquivo digital-7.ttf para ser nova fonte da calculcadora

--- app/src/main/java/com/germanoribeiro/calculatec/CalculadoraActivity.java
+++ app/src/main/java/com/germanoribeiro/calculatec/CalculadoraActivity.java
@@ -5,11 +5,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class CalculadoraActivity extends Activity {
     private Calculadora calc;
     private boolean usuarioEstaDigitandoUmNumero;
     private boolean separadorDecimalFoiDigitado;
     private TextView txtVisor;
+    private String separador;
+    private char separadorChar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -20,6 +28,17 @@ public class CalculadoraActivity extends Activity {
         separadorDecimalFoiDigitado = false;
         txtVisor = (TextView) findViewById(R.id.txtVisor);
         txtVisor.setText("0");
+        Locale localizacao = getResources().getConfiguration().locale;
+        NumberFormat formatador = NumberFormat.getInstance(localizacao);
+
+        separadorChar = ',';
+        if (formatador instanceof DecimalFormat){
+            DecimalFormatSymbols simbolo = ((DecimalFormat)formatador).getDecimalFormatSymbols();
+            separadorChar = simbolo.getDecimalSeparator();
+        }
+        separador = String.valueOf(separadorChar);
+        Button btnSeparador = (Button)findViewById(R.id.buttonVirgula);
+        btnSeparador.setText(separador);
     }
     public void onClickNumeros(View v){
         Button botaoTocado = (Button) v;
@@ -37,22 +56,23 @@ public class CalculadoraActivity extends Activity {
     public void onClickOperacoes(View v){
         Button botaoTocado = (Button) v;
         String operacao = botaoTocado.getText().toString();
-        if(operacao.equals(",") && !separadorDecimalFoiDigitado){
+
+        if(operacao.equals(separador) && !separadorDecimalFoiDigitado){
             separadorDecimalFoiDigitado = true;
             if(!usuarioEstaDigitandoUmNumero)
-                txtVisor.setText("0" +",");
+                txtVisor.setText("0" + separador);
             else
-                txtVisor.setText(txtVisor.getText().toString()+",");
+                txtVisor.setText(txtVisor.getText().toString() + separador);
             usuarioEstaDigitandoUmNumero = true;
-        } else if (!operacao.equals(",")) {
-            String valorSemVirgula = txtVisor.getText().toString().replace(',','.');
+        } else if (!operacao.equals(separador)) {
+            String valorSemVirgula = txtVisor.getText().toString().replace(separadorChar,'.');
             calc.setOperando(Double.parseDouble(valorSemVirgula));
             calc.realizarOperacao(operacao);
             String textoResultado = String.valueOf(calc.getOperando());
             if (textoResultado.endsWith(".0")){
                 textoResultado = textoResultado.substring(0, textoResultado.length()-2);
             }
-            txtVisor.setText(textoResultado.replace('.',','));
+            txtVisor.setText(textoResultado.replace('.', separadorChar));
             usuarioEstaDigitandoUmNumero = false;
             separadorDecimalFoiDigitado = false;
         }
@@ -60,7 +80,7 @@ public class CalculadoraActivity extends Activity {
     public void onClickMemoria(View v){
         Button botaoTocado = (Button) v;
         String operacaoMemoria = botaoTocado.getText().toString();
-        String valorSemVirgula = txtVisor.getText().toString().replace(',','.');
+        String valorSemVirgula = txtVisor.getText().toString().replace(separadorChar,'.');
         calc.setOperando(Double.parseDouble(valorSemVirgula));
         calc.realizarOperacaoDeMemoria(operacaoMemoria);
         usuarioEstaDigitandoUmNumero = false;
